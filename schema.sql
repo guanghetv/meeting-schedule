087fdef115a6af7e760ecefde13a7c37093e395f
YshadowZ <809077530@qq.com>
2017-11-15 14:03:01 +0800

    fix: 表结构设计

--- schema.sql
+++ schema.sql
@@ -7,9 +7,34 @@ create table meeting_room (
 
 create table meeting_schedule (
     "id" uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
-    "beginTime" timestamptz not null,
-    "endTime" timestamptz not null,
+    "timeRange" tstzrange NOT NULL,
     "userId" varchar(40),
     "description" text,
-    "roomId" smallint REFERENCES meeting_room (id) NOT NULL
+    "roomId" smallint REFERENCES meeting_room (id) NOT NULL,
+    "day" text not null,
+    "createTime" timestamptz DEFAULT now()
 );
+
+create or replace function check_time_cover() returns trigger as $time_cover_check$
+    declare
+        count int;
+    begin
+        count := (
+            select count(*) from meeting_schedule
+            where "timeRange" && NEW."timeRange"::tstzrange and "roomId" = NEW."roomId"
+        );
+        if (count > 0) then
+            raise exception 'time cover other';
+        else 
+            return new;
+        end if;
+    end;
+$time_cover_check$ language plpgsql;
+
+create trigger check_time_insert before insert on meeting_schedule 
+    for each row EXECUTE PROCEDURE check_time_cover();
+
+create trigger check_time_update before update on meeting_schedule
+    for each row EXECUTE PROCEDURE check_time_cover();
+
+-- insert into meeting_schedule ("timeRange", "userId", "description", "roomId", "day") values ('[2017-09-27 00:00:00+08,2017-10-13 00:00:00+08]', 'xxx', '描述', 1, '2017-10-10');
