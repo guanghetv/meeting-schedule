dda8687ed6af8c96607db0001367909179720043
lilinyi <LOVER12161@163.com>
2018-07-16 11:21:24 +0800

    更新登录界面

--- watchDog/src/pages/Login/login.jsx
+++ watchDog/src/pages/Login/login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Icon, Input, Button,message,notification } from 'antd';
+import { Form, Icon, Input, Button,message,notification,Checkbox } from 'antd';
 import { Redirect } from 'react-router-dom';
 const FormItem = Form.Item;
 import LoginMain from '../../../assets/images/logoMain.png';
@@ -11,7 +11,13 @@ class LoginForm extends React.Component {
             landed: false
         }
     }
-    handleSubmit=()=>{
+    handleSubmit=(e)=>{
+            e.preventDefault();
+            this.props.form.validateFields((err, values) => {
+              if (!err) {
+                console.log('Received values of form: ', values);
+              }
+            });
         this.setState({
             landed: true
         })
@@ -28,8 +34,28 @@ class LoginForm extends React.Component {
                         <p style={{color:'#ccc',letterSpacing:10,paddingBottom:15}}>会议室预定</p>
                     </h2>
                     <FormItem>
+                       {getFieldDecorator('userName', {
+                           rules: [{ required: true, message: 'Please input your username!' }],
+                        })(
+                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                        )}
+                    </FormItem>
+                    <FormItem>
+                       {getFieldDecorator('password', {
+                           rules: [{ required: true, message: 'Please input your Password!' }],
+                        })(
+                           <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('remember', {
+                            valuePropName: 'checked',
+                            initialValue: true,
+                        })(
+                        <Checkbox>记住密码</Checkbox>
+                        )}
                         <Button type="primary" htmlType="submit" className="login-form-button" style={{width:'100%'}}>
-                            进入
+                            登陆
                         </Button>
                     </FormItem>
                 </Form>
